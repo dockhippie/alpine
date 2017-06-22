eae82ffd52d733600a08c73a813ee4d86f197b9a
Thomas Boerger <thomas@webhippie.de>
2017-06-22 23:30:25 +0200

    Use upstream wait-for-it

--- update-tools.sh
+++ update-tools.sh
@@ -8,19 +8,9 @@ wget -O rootfs/usr/bin/templater \
 echo "> Make templater executable..."
 chmod +x rootfs/usr/bin/templater
 
-# echo "> Downloading wait-for-it..."
-# wget -O rootfs/usr/bin/wait-for-it \
-#   https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh
-
-#
-# Until https://github.com/vishnubob/wait-for-it/pull/25 gets merged or the
-# issue https://github.com/vishnubob/wait-for-it/issues/5 solved we are forced
-# to use the file from the PR, otherwise it won't work on Alpine.
-#
-
 echo "> Downloading wait-for-it..."
 wget -O rootfs/usr/bin/wait-for-it \
-  https://raw.githubusercontent.com/jdufner/wait-for-it/9730b3a4817084a2504a2f553b316cf211166acd/wait-for-it.sh
+  https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh
 
 echo "> Make wait-for-it executable..."
 chmod +x rootfs/usr/bin/wait-for-it
